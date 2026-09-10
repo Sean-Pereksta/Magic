287ef341e8b185dafa5b34ed3347b25dac721f26
Sean-Pereksta <sean.pereksta@gmail.com>
2026-09-09 23:50:53 -0400

    Update warrealms-cards-base.js

--- game/warrealms-pack/warrealms-cards-base.js
+++ game/warrealms-pack/warrealms-cards-base.js
@@ -131,7 +131,7 @@ export const CARDS = Object.freeze([
         excludeSelf: true
       }
     },
-    ally: { Trade: 1 },
+    ally: { trade: 1 },
     text: "Add 1 Heat to another friendly Heat card.",
     allyText: "Gain 1 Trade.",
     flavor: "To the Xythe, excess energy is merely potential without a vessel."
